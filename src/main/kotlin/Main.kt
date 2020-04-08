780a2db8bdaf3d57e91231cf95c424db52e8a3dc
Iancu Verghelet <iverghelet@yahoo.com>
2020-04-08 23:40:02 +0200

    add kotlinx.serialization example for user

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,14 +4,36 @@ import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.readText
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 
+@Serializable
+data class User(
+    val id: String,
+    val login: String,
+    val display_name: String,
+    val type: String,
+    val broadcaster_type: String,
+    val description: String,
+    val profile_image_url: String,
+    val offline_image_url: String,
+    val view_count: Long
+)
+
+
+@OptIn(UnstableDefault::class)
 suspend fun main() {
-    val videoId = "584303799"
+    val userId = "44322889"
     val clientId = "nyufzvu4k8h80iq0r7ya4zx1fsas7d"
-    val url = "https://api.twitch.tv/v5/videos/$videoId/comments"
+    val url = "https://api.twitch.tv/helix/users?id=$userId"
     val client = HttpClient(Apache)
     val response: HttpResponse = client.get(url) {
         header("Client-ID", clientId)
     }
-    println(response.readText())
+    val responseUserArray = Json.parseJson(response.readText()).jsonObject.getArray("data")
+    responseUserArray.forEach { e ->
+        println(Json.parse(User.serializer(),e.toString()))
+    }
 }
