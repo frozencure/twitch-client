5ac40057d6b4b5e0897a8e51318e7b7d14423b11
Iancu Verghelet <iverghelet@yahoo.com>
2020-08-19 14:17:32 +0200

    modify analytics docs

--- src/main/kotlin/samples/Analytics.kt
+++ src/main/kotlin/samples/Analytics.kt
@@ -7,10 +7,8 @@ import java.time.Duration
 import java.time.Instant
 
 
-val credentials = OAuthCredentials("4j4u933wfyaayp71pxilgqrva7693c", "nyufzvu4k8h80iq0r7ya4zx1fsas7d")
-
-
 fun getExtensionAnalytics() {
+    val credentials = OAuthCredentials("4j4u933wfyaayp71pxilgqrva7693c", "nyufzvu4k8h80iq0r7ya4zx1fsas7d")
     val helixClient = HelixClient(credentials)
     runBlocking {
         print(helixClient.analytics.getExtensionAnalytics(12345, Instant.now() - Duration.ofDays(1), Instant.now()))
@@ -18,6 +16,7 @@ fun getExtensionAnalytics() {
 }
 
 fun getGameAnalytics() {
+    val credentials = OAuthCredentials("4j4u933wfyaayp71pxilgqrva7693c", "nyufzvu4k8h80iq0r7ya4zx1fsas7d")
     val helixClient = HelixClient(credentials)
     runBlocking {
         print(helixClient.analytics.getGameAnalytics(12345, Instant.now() - Duration.ofDays(1), Instant.now()))
